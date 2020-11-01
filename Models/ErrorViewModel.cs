55d078173f1e6d7646a1950655c5187fad54a8e8
Erick <viajero33176@innovaccion.mx>
2020-11-01 10:27:51 -0800

    proyect creation

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace farma.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
