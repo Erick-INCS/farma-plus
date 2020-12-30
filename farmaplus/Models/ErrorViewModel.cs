0995b6177ca314ba0bbe9d5f4e9a80e49a21a095
FRANCISCO DIAZ <zightguitar@gmail.com>
2020-12-29 19:04:19 -0800

    New project

---
+++ farmaplus/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace farmaplus.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
